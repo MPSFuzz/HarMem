9cf6f1c375493f7679debc756544fa56354a59b8
Youngmith <1711231600@qq.com>
2025-08-22 17:37:13 +0800

    update

--- batch_manager.py
+++ batch_manager.py
@@ -1,9 +1,40 @@
-import hashlib
-import sqlite3
+import json
+import uuid
+import os
 
 from typing import List, Dict, Any
-from extract_call_chain import load_call_graph, get_root_apis, extract_target_call_chain
-from gen_hanress import get_available_harness
 
-def set_batch_info(lib_name: str, source_dir: str, dot_file: str, target_funcs: list):
-    return
+class Batch:
+    def __init__(self, batch_id: str=None ,target_func: str=None):
+        self.batch_id = str(uuid.uuid4())
+        self.target_func = target_func
+        self.json_path = ""
+        self.harness_info = {}
+
+    def save_metadata(self, filepath="./batch_metadata/") -> Any:
+        save_path = os.path.join(filepath, f"{self.batch_id}.json")
+        os.makedirs(filepath, exist_ok=True)
+
+        metadata = {
+            "batch_id": self.batch_id,
+            "target_func": self.target_func,
+            "harness_info": self.harness_info
+        }
+
+        with open(save_path, "w", encoding="utf-8") as f:
+            json.dump(metadata, f, indent=4)
+
+        self.json_path = save_path
+
+        return self.json_path
+    
+    @classmethod
+    def load_metadata(cls, file_path: str):
+        with open(file_path, "r", encoding="utf-8") as f:
+            metadata = json.load(f)
+
+        batch = cls(
+                batch_id = metadata["batch_id"],
+                target_func = metadata["tareget_func"]
+            )
+        batch.harness_info
